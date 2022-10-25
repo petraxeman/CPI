3a04f4a2e9611aaf5ea4fc8c499a63095953b22e
petraxeman <pashamanprofirus@gmail.com>
2022-10-25 17:55:36 +0300

    dev 1

--- cpp_pp/lexer.py
+++ cpp_pp/lexer.py
@@ -1,3 +1,4 @@
+import re
 from locale import currency
 from sre_parse import WHITESPACE
 
@@ -18,51 +19,107 @@ class Token:
 
 
 class Lexer:
-    def __init__(self, file_path: str,
-                 include_handler: 'IncludeHandler',
-                 tokenizer: 'Tokenizer' = None) -> None:
+    DERECTIVES = {'SD': [':', ';', '=', '-', '+', '*', '/', '%', '++', '--', '==', '!=',
+                         '>', '>=', '<', '<=', '!', '&&', '||', '~', '&', '|', '^', '<<', '>>',
+                         '(', ')', '[', ']', '{', '}', '+=', '-=', '*=', '/=', '%=', '|=', '^=',
+                         '<<=', '>>=', ' ', '\n', '\\', '\t', '\r', '#', ',', '/*', '*/'],
+                  'PPD': ['define', 'undefine', 'ifdef', 'ifndef'],
+                  'CPPD': ['char', 'char8_t', 'char16_t', 'char32_t', 
+                           'alignas', 'and', 'and_eq', 'asm', 'auto', 'bitand', 'bitor', 'bool', 'break', 'case', 'catch', 'class',
+                           'compl', 'concept', 'const', 'const_cast', 'consteval', 'constexpr', 'constinit', 'continiue', 'co_await',
+                           'co_return', 'co_yield', 'decltype', 'default', 'delete', 'do', 'double', 'dynamic_cast', 'else', 'enum',
+                           'explicit', 'export', 'extern', 'false', 'float', 'for', 'friend', 'goto', 'if', 'inline', 'int', 'long',
+                           'mutable', 'namespace', 'namespace', 'new', 'noexcept', 'not', 'not_eq', 'nullptr', 'operator', 'or', 'or_eq',
+                           'private', 'protected', 'public', 'requires', 'return', 'short', 'signed', 'sizeof', 'static', 'static_assert',
+                           'static_cast', 'struct', 'switch', 'template', 'this', 'thread_local', 'throw', 'true', 'try', 'typedef', 
+                           'typeid', 'typename', 'union', 'unsigned', 'using', 'virtual', 'void', 'volatile', 'wchar_t', 'while', 'xor', 'xor_eq']}
+    IS_FLOAT_RE = re.compile('^([+-]?[0-9]*\.?[0-9]*)$')
+    IS_INT_RE = re.compile('^([+-]?[0-9]\.?0?)$')
+    IS_STRING_RE = re.compile(r"""([bruf]*)(\"""|'''|"|')(?:(?!\2)(?:\\.|[^\\]))*\2""")
+    def __init__(self, 
+                 file_path: str,
+                 include_handler: 'IncludeHandler') -> None:
         self.root_file_path = file_path
         self.include_handler = include_handler
-        self.tokenizer = tokenizer or Tokenizer()
 
-    def token_generator(self) -> 'Token':
-        for token in self.tokenizer.process(text):
-            yield token
-
-
-class Tokenizer:
-    DELIMITER = list(r'#:(){}, ;"\'')
-    DELIMITER.append('\n')
-    ALPHABET = {'PPD': ['if', 'define', 'undefine', 'ifndef', 'ifdef'],
-                'CPPD': ['int', 'char', 'float', '<<', 'cout']}
-    WHITESPACE = [' ', '\t', '\n']
-    def __init__(self,
-                 delimiter: list[str] = None,
-                 alphabet: dict[str: list[str]] = None,
-                 whitespaces: list[str] = None) -> None:
-        self.DELIMITER = delimiter or self.DELIMITER
-        self.ALPHABET = alphabet or self.ALPHABET
-        self.WHITESPACE = whitespaces or self.WHITESPACE
-
-    def process(self, text: str) -> 'Token':
-        if text is None and self.text is None:
-            raise exceptions.TokenizerExpectsText('Tokenizer expects text. Got None.')
-
+    def process(self, text = None) -> 'Token':
+        if text is None:
+            with open(self.root_file_path, 'r', encoding='utf8') as file:
+                text = file.read()
+        
         current_token = ''
+        in_string = False; in_comment = False
 
         for char in text:
-            if char in self.WHITESPACE and current_token == char:
+            if current_token == '':
+                current_token += char
                 continue
-            elif current_token in self.WHITESPACE and current_token != char:
-                yield Token(current_token); current_token = ''
 
-            if current_token in self.DELIMITER and current_token not in self.WHITESPACE:
-                yield Token(current_token); current_token = ''
-
-            if char in self.DELIMITER:
-                if len(current_token) > 0:
-                    yield Token(current_token); current_token = ''
+            if current_token == '"' or current_token == "'":
+                in_string = True
             
-            
+            if in_comment:
+                if current_token + char == '*/':
+                    in_comment = False
+                    current_token = ''
+                    continue
+                current_token = char
+                continue
 
+            if in_string:
+                if char == '"' or char == "'":
+                    in_string = False
+                    current_token += char
+                    continue
+                current_token += char
+                continue
+
+            if char == ' ' and current_token == ' ':
+                continue
+            
+            if current_token + char == '/*':
+                in_comment = True
+                continue
+
+            if current_token + char in self.DERECTIVES['SD']:
+                yield Token(current_token + char, 'SD'); current_token = ''
+                continue
+            elif current_token in self.DERECTIVES['SD']:
+                yield Token(current_token, 'SD'); current_token = ''
+
+            for code in self.DERECTIVES:
+                if current_token in self.DERECTIVES[code]:
+                    if code != 'SD':
+                        yield Token(current_token, code); current_token = ''
+            
+            if char in self.DERECTIVES['SD']:
+                _is_num, _num_value = self.is_num(current_token)
+                if _is_num:
+                    yield Token(_num_value, 'NUM_CONST'); current_token = ''
+                
+                _is_str, _str_value = self.is_str(current_token)
+                if _is_str:
+                    yield Token(_str_value, 'STR_CONST'); current_token = ''
+                
+                if not _is_num and not _is_str:
+                    yield Token(current_token, 'ID'); current_token = ''
+            
             current_token += char
+
+    def is_num(self, string: str) -> bool or str:
+        if string == '':
+            return False, None
+        if len(re.findall(self.IS_INT_RE, string)) > 0:
+            return True, int(string)
+        elif len(re.findall(self.IS_FLOAT_RE, string)) > 0:
+            return True, float(string)
+        return False, None
+    
+    def is_str(self, string: str) -> bool or str:
+        if string == '':
+            return False, None
+        if len(re.findall(self.IS_STRING_RE, string)) > 0:
+            string = string.strip('"')
+            string = string.strip("'")
+            return True, string
+        return False, None
